c863eea2b47a793dad229c5a2f9ffd18decba855
magic chen <418379149@qq.com>
2022-06-24 15:31:55 +0800

    -- 优化继续爆破的参数输入方式 -- 可输入 num low high 选择使用字典模式

--- src/main/java/com/magic/Explode.java
+++ src/main/java/com/magic/Explode.java
@@ -10,37 +10,44 @@ import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public class Explode {
+
+    // 分别对应 0-9
+    private static List<String> num = Arrays.asList("0", "1", "2", "3", "4", "5", "6", "7", "8", "9");
+    // 分别对应 10 - 35
+    private static List<String> ch_low = Arrays.asList("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z");
+    // 分别对应 36 - 61
+    private static List<String> ch_high = Arrays.asList("A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z");
+
+
     //参考官方文档 https://pan.baidu.com/union/doc/Yksmyl2v0
     public static void main(String[] args) {
 
-        // 分别对应 0-9
-        List<String> num = Arrays.asList("0", "1", "2", "3", "4", "5", "6", "7", "8", "9");
-        // 分别对应 10 - 35
-        List<String> ch_low = Arrays.asList("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z");
-        // 分别对应 36 - 61
-        List<String> ch_high = Arrays.asList("A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z");
-        List<String> dic = new ArrayList<String>();
-        dic.addAll(num);
-        dic.addAll(ch_low);
-//        dic.addAll(ch_high); // 不清楚分享码带不带大写,可以先不带试试, 如果没试出来(建议放弃....36位 和 62位差距太大了)
-        // 读取运行参数,可以接着上次爆破进度继续爆破
+        String type = "low";
+        String last = "0000";
+
+        try {
+            type = args[0];
+            last = args[1];
+        } catch (Exception e) {
+        }
+
         int i, j, k, l;
         try {
-            i = Integer.valueOf(args[0]);
-            j = Integer.valueOf(args[1]);
-            k = Integer.valueOf(args[2]);
-            l = Integer.valueOf(args[3]);
+            Map<String, Integer> dicMap = initDicMap();
+            i = dicMap.get(String.valueOf(last.charAt(0)));
+            j = dicMap.get(String.valueOf(last.charAt(1)));
+            k = dicMap.get(String.valueOf(last.charAt(2)));
+            l = dicMap.get(String.valueOf(last.charAt(3)));
         } catch (Exception e) {
             i = 0;
             j = 0;
             k = 0;
             l = 0;
         }
+        List<String> dic = getKeys(type);
         System.out.println("起始位: " + dic.get(i) + "," + dic.get(j) + "," + dic.get(k) + "," + dic.get(l));
 
         RestTemplate restTemplate = new RestTemplate();
@@ -110,8 +117,8 @@ public class Explode {
         } catch (HttpClientErrorException e) {
             if (e.getStatusCode() == HttpStatus.NOT_FOUND) {
                 try {
-                    // 10分钟后继续
-                    Thread.sleep(1000 * 60 * 10L);
+                    // 半小时后继续
+                    Thread.sleep(1000 * 60 * 30L);
                     result = postRecursion(restTemplate, url, request);
                 } catch (InterruptedException ie) {
                     return "Error sleep interrupted";//
@@ -122,6 +129,46 @@ public class Explode {
             }
         }
         return result;
+    }
+
+
+    // 使用字典的规格
+    // 不清楚分享码带不带大写,可以先不带试试, 如果没试出来(建议放弃....36位 和 62位差距太大了)
+    public static List<String> getKeys(String type) {
+        List<String> dic = new ArrayList<String>();
+        if (type.equals("num")) {
+            dic.addAll(num);
+        } else if (type.equals("low")) {
+            dic.addAll(num);
+            dic.addAll(ch_low);
+        } else if (type.equals("high")) {
+            dic.addAll(num);
+            dic.addAll(ch_low);
+            dic.addAll(ch_high);
+        } else {
+            dic.addAll(num);
+            dic.addAll(ch_low);
+        }
+        return dic;
+    }
+
+    // 继续爆破的字符 对应 便利数字
+    public static Map<String, Integer> initDicMap() {
+        Map<String, Integer> dicMap = new HashMap<>();
+        int i = 0;
+        for (String s : num) {
+            dicMap.put(s, i);
+            i++;
+        }
+        for (String s : ch_low) {
+            dicMap.put(s, i);
+            i++;
+        }
+        for (String s : ch_high) {
+            dicMap.put(s, i);
+            i++;
+        }
+        return dicMap;
     }
 
 
